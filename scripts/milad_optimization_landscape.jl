18c584ddacd73d3d5be8bcd5c769de912d2f1d9c
Spencer Lee <leespen1@msu.edu>
2025-06-02 21:08:57 -0700

    Made more suitable for HPC

--- scripts/milad_optimization_landscape.jl
+++ scripts/milad_optimization_landscape.jl
@@ -1,10 +1,47 @@
 using DrWatson
 quickactivate(pwd(), "JuliaPulseExperiments")
 
-using QuantumToolbox, GLMakie, Random
+using QuantumToolbox, Yao, Random, ProgressMeter
 
 includet(srcdir("wasserstein_distance.jl"))
 includet(srcdir("milad_circuit.jl"))
+
+"""
+Iterate over theta2 for fixed theta1. Get Infidelity and W1 distances as vectors.
+"""
+function makesim(d::Dict)
+    @unpack Nqubits, i1, i2, theta1, Npoints = d
+    angles = rand(MersenneTwister(0), Nqubits) .* 2pi
+    angles[1] = pi
+    angles[i1] = theta1
+
+    theta2range = LinRange(0, 2pi, Npoints)
+    ghz_dm = ghz_operator(Nqubits)
+
+    infidelity_vec = Vector{Float64}(undef, Npoints)
+    W1_vec = Vector{Float64}(undef, Npoints)
+
+    for (k, theta2) in enumerate(theta2range)
+        angles[i2] = theta2
+        final_state = run_milad_circuit(angles) |> statevec
+
+        infidelity_vec[k] = ghz_infidelity(final_state)
+
+        dims = ntuple(_ -> 2, Nqubits)
+        final_dm = Qobj(final_state, dims=dims) |> ket2dm
+        W1_vec[k] = W1_primal(final_dm, ghz_dm)
+
+        #GC.gc() # Being safe about running out of memory
+    end
+
+    fulld = Dict{String, Any}(copy(d))
+    fulld["angles"] = angles
+    fulld["theta2range"] = theta2range
+    fulld["infidelity_vec"] = infidelity_vec
+    fulld["W1_vec"] = W1_vec
+
+    return fulld
+end
 
 """
 Only really optimal when Nqubits % 4 == 0
@@ -26,68 +63,24 @@ function run_milad_circuit_operator(angles)
     return Qobj(circuit_output_vec, dims=dims)  |> ket2dm
 end
 
-function collect_data(Nqubits::Integer, i1::Integer, i2::Integer;
-        i1_lims=(0,2pi),  i2_lims=(0,2pi), N_points=101, obj_type=:infidelity)
-    # Q | Better to set non-changing angles as optimal or nonoptimal?
-    # A | It's better to do non-optimal. If almost all angles are optimal, we don't see barren plateau
-    #angles = optimal_angles(Nqubits)
-    angles = rand(MersenneTwister(0), Nqubits) .* 2pi # Random in range [0,2pi]
-    #angles .= pi
-    angles[1] = pi # Maybe just θ₁ optimal is good
-
-    ghz_dm = ghz_operator(Nqubits)
-
-    i1_linrange = LinRange(i1_lims..., N_points) 
-    i2_linrange = LinRange(i2_lims..., N_points) 
-    obj_matrix = Matrix{Float64}(undef, N_points, N_points)
-    for (j1, theta_i1) in enumerate(i1_linrange)
-        println("Doing row #", j1)
-        for (j2, theta_i2) in enumerate(i2_linrange)
-            angles[i1] = theta_i1
-            angles[i2] = theta_i2
-
-            if obj_type == :infidelity
-                obj_matrix[j1,j2] = infidelity_obj(angles)
-            elseif obj_type == :w1_primal
-                final_dm = run_milad_circuit_operator(angles)
-                obj_matrix[j1,j2] = W1_primal(final_dm, ghz_dm)
-            elseif obj_type == :w1_dual
-                final_dm = run_milad_circuit_operator(angles)
-                obj_matrix[j1,j2] = W1_dual(final_dm, ghz_dm)
-            else
-                error("Invalid objective type '$obj_type'")
-            end
-        end
-    end
-    return obj_matrix 
-end
-
 
 function main(obj_type=:infidelity)
-    N_points = 26
-    N_cols = 2
-    hms = []
-    fig = Figure()
+    Npoints = 11
+    allparams = Dict{String, Any}(
+        "Nqubits" => collect(3:4),
+        "i1" => [2],
+        "i2" => [3],
+        "theta1" => collect(LinRange(0,2pi,Npoints)),
+        "Npoints" => [Npoints],
+    )
 
-    for (i, Nqubits) in enumerate(3:6)
-        data = collect_data(Nqubits, 2, 3, N_points=N_points,
-                                      obj_type=obj_type)
-        ax_tmp, hm_tmp = GLMakie.heatmap(
-            fig[1,end+2], (0,2pi), (0,2pi), data;
-            colorscale=log10,
-            colorrange=(0.5, 1),
-            axis=(; title="$Nqubits Qubits"),
-
-        )
-        @show i, minimum(data)
-        push!(hms, hm_tmp)
+    dicts = dict_list(allparams)
+    @showprogress for d in dicts
+        f = makesim(d)
+        #wsave(datadir("MiladCircuitDistances", savename(d, "jld2")), f)
+        produce_or_load(makesim, d, datadir("MiladCircuitDistances"), loadfile=false)
+        #wsave(datadir("MiladCircuitDistances", savename(d, "jld2")), f)
     end
-    for i in 1:length(hms)
-        Colorbar(fig[1,2i], hms[i])
-    end
-    return fig
 end
 
-#fig = main()
-
-# TODO Want to show barren landscape. Should I set θ₁ optimally? What is optimal?
+main()
