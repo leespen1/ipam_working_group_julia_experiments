df24fb621448bf127a5249fdfccf9a9b70f8fb14
Spencer Lee <leespen1@msu.edu>
2025-06-04 11:41:10 -0700

    Added script for plotting heatmaps

---
+++ scripts/plot_heatmap_milad_landscape.jl
@@ -0,0 +1,47 @@
+using DrWatson
+quickactivate(pwd(), "JuliaPulseExperiments")
+
+using DataFrames, DataFrameMacros, CairoMakie
+
+
+df = collect_results(datadir("MiladCircuitHeatmaps"))
+
+mkpath(plotsdir("MiladCircuitHeatmaps")) # Where we will store plots
+
+for df_nqubits in @groupby(df, :Nqubits, :i1, :i2, :optimizer)
+    @assert size(df_nqubits, 1) == 1
+    Nqubits = df_nqubits[1,:Nqubits]
+    i1 = df_nqubits[1,:i1]
+    i2 = df_nqubits[1,:i2]
+    optimizer = df_nqubits[1,:optimizer]
+    x = df_nqubits[1,:theta1s]
+    y = df_nqubits[1,:theta2s]
+    data_inf = df_nqubits[1,:data_infidelity]
+    data_W1 = df_nqubits[1,:data_W1]
+
+
+    dict_inf = @dict(Nqubits, i1, i2, optimizer)
+    dict_W1 = @dict(Nqubits, i1, i2, optimizer)
+    dict_inf[:cost] = "infidelity"
+    dict_W1[:cost] = "W1"
+
+    fig_inf = Figure()
+    ax_inf = Axis(
+        fig_inf[1,1], xlabel="theta $i1", ylabel="theta $i2",
+        title=savename(dict_inf, connector=", ")
+    )
+    hm_inf = heatmap!(ax_inf, x, y, data_inf)
+    Colorbar(fig_inf[1,end+1], hm_inf)
+
+    fig_W1 = Figure()
+    ax_W1 = Axis(
+        fig_W1[1,1], xlabel="theta $i1", ylabel="theta $i2",
+        title=savename(dict_W1, connector=", ")
+    )
+    hm_W1 = heatmap!(ax_W1, x, y, data_W1)
+    Colorbar(fig_W1[1,end+1], hm_W1)
+
+    save(plotsdir("MiladCircuitHeatmaps", savename(dict_inf, "png")), fig_inf)
+    save(plotsdir("MiladCircuitHeatmaps", savename(dict_W1, "png")), fig_W1)
+end
+
